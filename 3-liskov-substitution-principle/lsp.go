70727989264bc49fb17e155488dbe4b04595ed45
Kirill Kotliarov <kirill.kotliarov@synesis.ru>
2017-08-03 15:24:37 +0300

    solid: lsp

---
+++ 3-liskov-substitution-principle/lsp.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// Liskov ...
+type Liskov interface {
+	LiskovFunc()
+}
+
+// Parent ...
+type Parent struct{}
+
+// Child ...
+type Child struct {
+	Parent
+}
+
+// LiskovFunc ...
+func (p *Parent) LiskovFunc() {
+	fmt.Println("It works")
+}
+
+// LiskovSubstitution ...
+func LiskovSubstitution(lis Liskov) {
+	lis.LiskovFunc()
+}
+
+func main() {
+	ch := &Child{}
+	par := &Parent{}
+	LiskovSubstitution(ch)
+	LiskovSubstitution(par)
+}
