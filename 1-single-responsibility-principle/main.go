6f5e8248e06d5de5616b27cefde398f801e6ad29
Kirill Kotliarov <kirill.kotliarov@synesis.ru>
2017-08-07 12:55:40 +0300

    solid: srp

---
+++ 1-single-responsibility-principle/main.go
@@ -0,0 +1,87 @@
+// Stupid example for realize principle of Single responsibility
+// bad part
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// ReportBad ...
+type ReportBad struct {
+	Title string
+	Date  string
+}
+
+func (r *ReportBad) getTitle() string {
+	return r.Title
+}
+
+func (r *ReportBad) getDate() string {
+	return r.Date
+}
+
+func (r *ReportBad) getContents() (string, string) {
+	return r.Title, r.Date
+}
+
+func (r *ReportBad) formatJSON() ([]byte, error) {
+	answer, err := json.Marshal(r)
+	if err != nil {
+		return nil, fmt.Errorf("Method: formatJSON, err: %v", err)
+	}
+
+	return answer, nil
+}
+
+// good part
+
+// ReportGood ...
+type ReportGood struct {
+	Title string
+	Date  string
+}
+
+func (r *ReportGood) getTitle() string {
+	return r.Title
+}
+
+func (r *ReportGood) getDate() string {
+	return r.Date
+}
+
+func (r *ReportGood) getContents() (string, string) {
+	return r.Title, r.Date
+}
+
+// ReportFormat ...
+type ReportFormat struct {
+}
+
+func (rf *ReportFormat) formatJSON(r *ReportGood) ([]byte, error) {
+	answer, err := json.Marshal(r)
+	if err != nil {
+		return nil, fmt.Errorf("Method: formatJSON, err: %v", err)
+	}
+
+	return answer, nil
+}
+
+func main() {
+	repBad := &ReportBad{Date: "123", Title: "Bad"}
+	ansBad, err := repBad.formatJSON()
+	if err != nil {
+		panic(err)
+	}
+	os.Stdout.Write(ansBad)
+	fmt.Println()
+
+	repGood := &ReportGood{Date: "321", Title: "Good"}
+	ansGood, err := (&ReportFormat{}).formatJSON(repGood)
+	if err != nil {
+		panic(err)
+	}
+	os.Stdout.Write(ansGood)
+	fmt.Println()
+}
